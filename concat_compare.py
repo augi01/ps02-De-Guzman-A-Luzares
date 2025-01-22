a5fc29668ac36471518aeb2bd4ae22186d0f6b5c
Augi01 <augifrancodeguzman01@gmail.com>
2025-01-22 11:51:51 +0800

    Update concat_compare.py
    code 2 done

--- concat_compare.py
+++ concat_compare.py
@@ -1,3 +1,13 @@
+import time
+
+# Original Method
+start = time.time()
+result = ""
+for i in range(1, 100001):
+    result += str(i)
+print("+= Time:", time.time() - start)
+
+
 # Join Method
 start = time.time()
 result = "".join(str(i) for i in range(1, 100001))

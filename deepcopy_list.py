2a01c2c0ed7ed6430c64ee2515274909859bef64
Augi01 <augifrancodeguzman01@gmail.com>
2025-01-22 12:10:07 +0800

    Update deepcopy_list.py
    code 4 done

--- deepcopy_list.py
+++ deepcopy_list.py
@@ -1,1 +1,12 @@
+import copy
 
+nested_list = [[1, 2], [3, 4]]
+alias_nested = nested_list
+alias_nested[0][1] = 99
+
+deep_copied_list = copy.deepcopy(nested_list)
+deep_copied_list[0][1] = 2 
+
+print("Original nested list:", nested_list)
+print("Aliased nested list:", alias_nested)
+print("Deep copied list:", deep_copied_list)
